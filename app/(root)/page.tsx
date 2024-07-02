01c1ef1eb44514bf2c8b1d94e82edb8d55a16fc7
Jordyn Jensen <jordyneajensen@gmail.com>
2024-07-01 19:47:47 -0500

    Made HeaderBox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Jordyn' };
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting" 
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your accound and transactions efficiently."         
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
