2e6936ed9b70d1eb6fa1570d19d265cb5bb733a7
Jordyn Jensen <jordyneajensen@gmail.com>
2024-07-03 13:05:47 -0500

    created Sidebar component

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -14,7 +14,7 @@ const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
             {
                 label: 'Banks',
                 data: balances,
-                backgroundColor: ['#0747b6, #2265d8', '#2f91fa']
+                backgroundColor: ['#0747b6', '#2265d8', '#2f91fa']
             }
         ],
         labels: accountNames
