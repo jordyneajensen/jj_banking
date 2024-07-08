7fec0a6cfdf603a364a20da86f9bff43574982ad
Jordyn Jensen <jordyneajensen@gmail.com>
2024-07-08 15:48:20 -0500

    completed sidebar for now

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
   return (
     <section className='sidebar'>
         <nav className='flex flex-col gap-4'>
-            <Link href='/' className='mb-12 cursor-pointer items-center gap-2'>
+            <Link href='/' className='mb-12 cursor-pointer flex items-center gap-2'>
                 <Image 
                     src="/icons/logo.svg"
                     width={34}
@@ -27,8 +27,21 @@ const Sidebar = ({ user }: SiderbarProps) => {
             {sidebarLinks.map((item) => {
                 const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
                 return(
-                    <Link href={item.route} key={item.label} className={cn('sidebar-link', {'bg-bank-gradient': isActive})}>
-                        {item.label}
+                    <Link 
+                        href={item.route} key={item.label} className={cn('sidebar-link', {'bg-bank-gradient': isActive})}
+                    >
+                        <div className='relative size-6'>
+                            <Image 
+                                src={item.imgURL}
+                                alt={item.label}
+                                fill
+                                className={cn({'brightness-[3] invert-0': isActive
+                                })}
+                            />
+                        </div>
+                        <p className={cn('sidebar-label', {'!text-white': isActive})}>
+                            {item.label}
+                        </p>
                     </Link>
                 )
             })}
