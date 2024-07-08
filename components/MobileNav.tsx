9122d26a5917b74499b3f2f89f233145507ab7cf
Jordyn Jensen <jordyneajensen@gmail.com>
2024-07-08 16:03:32 -0500

    installed shadcn sheets and created hamburger menu

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from 'next/image'
+
+  
 
 const MobileNav = ({ user }: MobileNavProps) => {
   return (
-    <div>MobileNav</div>
+    <section className='w-full max-w-[264px]'>
+        <Sheet>
+            <SheetTrigger>
+                <Image 
+                    src='/icons/hamburger.svg'
+                    width={30}
+                    height={30}
+                    alt='menu'
+                    className='cursor-pointer'
+                />
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Are you absolutely sure?</SheetTitle>
+                    <SheetDescription>
+                        This action cannot be undone. This will permanently delete your account
+                        and remove your data from our servers.
+                    </SheetDescription>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+  </section>
   )
 }
 
