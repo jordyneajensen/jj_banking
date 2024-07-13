07ff4f169aa391563f9ea82cae822ad35ef9d0e7
Jordyn Jensen <jordyneajensen@gmail.com>
2024-07-13 14:55:14 -0500

    created onSubmit funcationality

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,10 +22,12 @@ import { Input } from "@/components/ui/input"
 import CustomInputs from './CustomInputs';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 const AuthForm = ({ type }: { type: string }) => {
-    const [user, setUser] = useState(null)
-    const [isLoading, setIsLoading] = useState(false)
+    const router = useRouter();
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     const formSchema = authFormSchema(type)
 
@@ -38,13 +40,45 @@ const AuthForm = ({ type }: { type: string }) => {
         })
         
         // 2. Define a submit handler.
-        function onSubmit(values: z.infer<typeof formSchema>) {
-            // Do something with the form values.
-            // ✅ This will be type-safe and validated.
-            setIsLoading(true)
-            console.log(values)
-            setIsLoading(false);
-        }
+        const onSubmit = async (data: z.infer<typeof formSchema>) => {
+            setIsLoading(true);
+      
+            try {
+              // Sign up with Appwrite & create plaid token
+              
+              if(type === 'sign-up') {
+                // const userData = {
+                //   firstName: data.firstName!,
+                //   lastName: data.lastName!,
+                //   address1: data.address1!,
+                //   city: data.city!,
+                //   state: data.state!,
+                //   postalCode: data.postalCode!,
+                //   dateOfBirth: data.dateOfBirth!,
+                //   ssn: data.ssn!,
+                //   email: data.email,
+                //   password: data.password
+                // }
+      
+                // const newUser = await signUp(userData);
+      
+                // setUser(newUser);
+              }
+      
+              if(type === 'sign-in') {
+                // const response = await signIn({
+                //   email: data.email,
+                //   password: data.password,
+                // })
+      
+                // if(response) router.push('/')
+              }
+            } catch (error) {
+              console.log(error);
+            } finally {
+              setIsLoading(false);
+            }
+          }
 
   return (
     <section className='auth-form'>
