d2a63ecb734ce1355dd02d8b5982913870faa21d
Jordyn Jensen <jordyneajensen@gmail.com>
2024-07-16 13:56:48 -0500

    edit

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,12 +23,13 @@ import CustomInputs from './CustomInputs';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { signIn, signUp } from '@/lib/actions/user.actions';
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
 
-const AuthForm = ({ type }: { type: string }) => {
+const AuthForm = async ({ type }: { type: string }) => {
     const router = useRouter();
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    const loggedInUser = await getLoggedInUser();
 
     const formSchema = authFormSchema(type)
 
