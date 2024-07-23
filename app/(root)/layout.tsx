73361ea07b519e3c0b053a153ac541b247594849
Jordyn Jensen <jordyneajensen@gmail.com>
2024-07-23 12:13:46 -0500

    finished footer component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,15 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
 
-  const loggedIn = { firstName: 'Jordyn', lastName: 'Jensen' };
+  const loggedIn = await getLoggedInUser();
 
   return (
     <main className="flex h-screen w-full font-inter">
@@ -17,7 +18,7 @@ export default function RootLayout({
           <div className="root-layout">
             <Image src='/icons/logo.svg' width={30} height={30} alt='menu icon'/>
             <div>
-              <MobileNav user={loggedIN}/>
+              <MobileNav user={loggedIn}/>
             </div>
           </div>
           {children}
