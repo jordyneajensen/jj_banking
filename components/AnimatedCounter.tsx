a1903646e81bb7d04794f23b7effa54dc3bbc50b
Jordyn Jensen <jordyneajensen@gmail.com>
2024-08-25 14:33:55 -0500

    fixed async/await client side issue with use effect

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -1,17 +1,16 @@
-'use client'
-import CountUp from 'react-countup/build/CountUp'
-import React from 'react'
+'use client';
 
-const AnimatedCounter = ({ amount }: { amount: number}) => {
+import CountUp from 'react-countup';
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
-    <div className='w-full'>
-        <CountUp 
-            duration={2.75}
-            decimals={2}
-            decimal=','
-            prefix='$'
-            end={amount}
-        />
+    <div className="w-full">
+      <CountUp 
+        decimals={2}
+        decimal=","
+        prefix="$"
+        end={amount} 
+      />
     </div>
   )
 }
